4e29b614a9fdd2b73cead0e5bdffb522983d9452
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2017-07-19 01:32:12 -0700

    update app to reflect changes to lib

--- tensorsoup/apps/matchlstm_squad/app.py
+++ tensorsoup/apps/matchlstm_squad/app.py
@@ -1,24 +1,31 @@
 import tensorflow as tf
-
 import sys
+
 sys.path.append('../../')
 
 from models.matchlstm.model import MatchLSTM
 from tasks.squad.data import DataSource
 from train.trainer import Trainer
 
+from multigpu import make_parallel
+
 
 if __name__ == '__main__':
-    d = 150 # hidden dim
+    
     batch_size = 30
+
     # instantiate model
-    model = MatchLSTM(emb_dim=200, hidden_dim=d, lr=0.001)
+    model = MatchLSTM(emb_dim=300, hidden_dim=200)
+
+    # make 'n' copies of model for data parallelism
+    make_parallel(model, num_copies=8, num_gpus=4)
+
     # create data source (SQuAD)
-    datasrc = DataSource(batch_size, glove_file='../../../datasets/glove/glove.6B.200d.txt')
+    datasrc = DataSource(batch_size, glove_file='../../../datasets/glove/glove.6B.300d.txt')
 
     # gpu config
     config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+    #config.gpu_options.allow_growth = True
 
     with tf.Session(config=config) as sess:
         # init session
@@ -28,4 +35,4 @@ if __name__ == '__main__':
         trainer = Trainer(sess, model, datasrc, batch_size)
 
         # fit model
-        trainer.fit(epochs=30)
+        trainer.fit(epochs=1000)
