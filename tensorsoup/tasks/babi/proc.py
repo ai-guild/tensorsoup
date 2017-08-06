c79f00d6ebd41ea5230ae96a6b10cc60870d613c
M Selvakumar <selva.developer@gmail.com>
2017-08-06 14:45:27 +0530

    preprocessing a data for single task complete

--- tensorsoup/tasks/babi/proc.py
+++ tensorsoup/tasks/babi/proc.py
@@ -1,120 +1,208 @@
-from __future__ import absolute_import
-
 import os
-import re
-import numpy as np
+import sys
 
-def load_task(data_dir, task_id, only_supporting=False):
-    '''Load the nth task. There are 20 tasks in total.
+sys.path.append('../../')
 
-    Returns a tuple containing the training and testing data for the task.
-    '''
-    assert task_id > 0 and task_id < 21
-
-    files = os.listdir(data_dir)
-    files = [os.path.join(data_dir, f) for f in files]
-    s = 'qa{}_'.format(task_id)
-    train_file = [f for f in files if s in f and 'train' in f][0]
-    test_file = [f for f in files if s in f and 'test' in f][0]
-    train_data = get_stories(train_file, only_supporting)
-    test_data = get_stories(test_file, only_supporting)
-    return train_data, test_data
-
-def tokenize(sent):
-    '''Return the tokens of a sentence including punctuation.
-    >>> tokenize('Bob dropped the apple. Where is the apple?')
-    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
-    '''
-    return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+from tproc.utils import *
 
 
-def parse_stories(lines, only_supporting=False):
-    '''Parse stories provided in the bAbI tasks format
-    If only_supporting is true, only the sentences that support the answer are kept.
-    '''
-    data = []
-    story = []
-    for line in lines:
-        line = str.lower(line)
-        nid, line = line.split(' ', 1)
-        nid = int(nid)
-        if nid == 1:
-            story = []
-        if '\t' in line: # question
-            q, a, supporting = line.split('\t')
-            q = tokenize(q)
-            #a = tokenize(a)
-            # answer is one vocab word even if it's actually multiple words
-            a = [a]
-            substory = None
+DATA_DIR_1K = '../../../datasets/babi/en/'
+DATA_DIR_10K = '../../../datasets/babi/en-10k/'
 
-            # remove question marks
-            if q[-1] == "?":
-                q = q[:-1]
 
-            if only_supporting:
-                # Only select the related substory
-                supporting = map(int, supporting.split())
-                substory = [story[i - 1] for i in supporting]
+def read_from_file(filename):
+    with open(filename) as f:
+        return [ line for line in f.read().split('\n') if line ]
+
+def fetch_data(filename):
+    contexts, answers, supporting_facts = [], [], []
+    questions = []
+    context = []
+
+    offset = 0
+
+    # read from file
+    lines = read_from_file(filename)
+    sample = []
+    samples = []
+    for i, line in enumerate(lines):
+        index = int(line.split(' ')[0])
+        if index == 1:
+            samples.append(sample)
+            sample = [line]
+        else:
+            sample.append(line)
+
+    samples = samples[1:]
+    verbose = True
+    def printv(*s):
+        if verbose == True:
+            print(*s)
+            
+    def process_sample(sample):
+
+        def sanitize(s):
+            s = ' '.join(s.split(' ')[1:])
+            return str(s).strip('.? \t\n')
+            
+        def process_support_old(support):
+            s =  support.split(' ')
+            s = [sample_dict[i] for i in s]
+            
+            return [local_context.index(i) for i in s]
+
+        def process_support(support, debug=False):
+            s = support.split(' ')
+            s = [int(i) for i in s]
+            _support = []
+            for j in s:
+                for i, line in enumerate(local_context):
+                    index = int(line.split(' ')[0])
+                    #printv(i, j, index, line)
+                    if j == index:
+                        if debug:
+                            _support.append((i, j))
+                        else:
+                            _support.append(i)
+                        break
+
+                        
+            #printv(_support)
+            #printv('=================')
+            return _support
+        
+        contexts, questions, answers, supports = [], [], [], []
+
+        local_context = []
+        sample_dict  = {}
+        for line in sample:
+            index = line.split(' ')[0]
+            sample_dict[index] = line
+
+            if '?' in line:
+                contexts.append(local_context[:])
+                question, answer, support = line.split('\t')
+                question = question[:question.index('?')]
+
+                support = process_support(support)
+
+                questions.append(question)
+                answers.append(answer)
+                supports.append(support)
+                
             else:
-                # Provide all the substories
-                substory = [x for x in story if x]
+                local_context.append(line)
 
-            data.append((substory, q, a))
-            story.append('')
-        else: # regular sentence
-            # remove periods
-            sent = tokenize(line)
-            if sent[-1] == ".":
-                sent = sent[:-1]
-            story.append(sent)
-    return data
+        contexts = [[sanitize(s) for s in context] for context in contexts]
+        questions = [sanitize(question) for question in questions]
+        
+        return contexts, questions, answers, supports
 
+    contexts, questions, answers, supports = [], [], [], []
+    temp  = 0
+    from pprint import pprint
+    for sample in samples:
+        c, q, a, s = process_sample(sample)
+        print('len ==>', len(c), len(q), len(a), len(s))
+        pprint([c, q, a, s])
+        print('########')
+        contexts.extend(c)
+        questions.extend(q)
+        answers.extend(a)
+        supports.extend(s)
 
-def get_stories(f, only_supporting=False):
-    '''Given a file name, read the file, retrieve the stories, and then convert the sentences into a single story.
-    If max_length is supplied, any stories longer than max_length tokens will be discarded.
-    '''
-    with open(f) as f:
-        return parse_stories(f.readlines(), only_supporting=only_supporting)
-
-def vectorize_data(data, word_idx, sentence_size, memory_size):
-    """
-    Vectorize stories and queries.
-
-    If a sentence length < sentence_size, the sentence will be padded with 0's.
-
-    If a story length < memory_size, the story will be padded with empty memories.
-    Empty memories are 1-D arrays of length sentence_size filled with 0's.
-
-    The answer array is returned as a one-hot encoding.
-    """
-    S = []
-    Q = []
-    A = []
-    for story, query, answer in data:
-        ss = []
-        for i, sentence in enumerate(story, 1):
-            ls = max(0, sentence_size - len(sentence))
-            ss.append([word_idx[w] for w in sentence] + [0] * ls)
-
-        # take only the most recent sentences that fit in memory
-        ss = ss[::-1][:memory_size][::-1]
-
-        # pad to memory_size
-        lm = max(0, memory_size - len(ss))
-        for _ in range(lm):
-            ss.append([0] * sentence_size)
-
-        lq = max(0, sentence_size - len(query))
-        q = [word_idx[w] for w in query] + [0] * lq
-
-        #y = np.zeros(len(word_idx) + 1) # 0 is reserved for nil word
-        #for a in answer:
-        #    y[word_idx[a]] = 1
+    return contexts, questions, answers, supports
 
 
-        S.append(ss)
-        Q.append(q)
-        A.append(word_idx[answer[0]])
-    return np.array(S), np.array(Q), np.array(A)
+    
+def fetch_data1(filename):
+
+    contexts, answers, supporting_facts = [], [], []
+    questions = []
+    context = []
+
+    offset = 0
+
+    # read from file
+    lines = read_from_file(filename)
+    for i,line in enumerate(lines):
+        # get line id
+        line_id = int(line.split(' ')[0])
+
+        # remove period
+        context_i = line[:-1].split(' ')[1:] if line[-1] == '.' else line
+
+        if '?' in line:
+            copy_of = context[:]
+            contexts.append(copy_of)
+
+            # process line
+            #  remove index from start of line
+            qline = ' '.join(line.split(' ')[1:])
+
+            # split line into question, answer 
+            #  and supporting facts indices
+            question, answer, sfacts = qline.split('\t')
+
+            #  remove question mark
+            question = question[:question.index('?')]
+
+            # add to corresponding lists
+            questions.append(question.split(' '))
+            answers.append(answer)
+
+            offset += 1
+
+            # convert supporting fact indices to integers
+            sfacts = [ int(j)-offset-1 for j in sfacts.split(' ') if j ]
+            supporting_facts.append(sfacts)
+
+        else:
+            # add line to local context
+            context.append(context_i)
+
+        # if next line id is less than current
+        if i+1 < len(lines) and int(lines[i+1].split(' ')[0]) < line_id:
+            context = []
+            offset = 0
+
+        '''
+        # remove index (from start of line)
+        #  add to current context
+        if '?' in line:
+            # process line
+            #  remove index from start of line
+            qline = ' '.join(line.split(' ')[1:])
+            # split line into question, answer 
+            #  and supporting facts indices
+            question, answer, sfacts = qline.split('\t')
+            #  [1] remove question mark
+            question = question[:question.index('?')]
+            # add to corresponding lists
+            questions.append(question.split(' '))
+            answers.append(answer)
+            # convert supporting fact indices to integers
+            sfacts = [ int(j)-1 for j in sfacts.split(' ') if j ]
+            supporting_facts.append(sfacts)
+
+            copy_of_context = context[:]
+            contexts.append(copy_of_context)
+        
+            if line_id == m:
+                context = []
+
+        else:
+            context.append(context_i.split(' ')[1:])
+
+        '''
+
+    return contexts, questions, answers, supporting_facts
+
+def process():
+
+    # get list of files
+    files = list_of_files(DATA_DIR_1K)
+
+    #raw_data = [ fetch_data(f) for f in files ]
+
+    return fetch_data1(files[0])
