935356b9573cd6bbb50c76bed0b294e1a25ad4ce
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2017-08-09 22:27:22 +0530

    update app to reflect changes

--- tensorsoup/apps/babi/memnet.py
+++ tensorsoup/apps/babi/memnet.py
@@ -13,46 +13,75 @@ from visualizer import Visualizer
 
 if __name__ == '__main__':
 
-    batch_size = 128
+    batch_size = 64
 
-    # get task 1
-    task = 1
-    data, metadata = gather('1k', task)
+    task_max_acc = []
+    for task in range(1,21):
+        # get task 1
+        #task = 18
+        data, metadata = gather('1k', task)
 
-    # gather info from metadata
-    num_candidates = metadata['candidates']['vocab_size']
-    vocab_size = metadata['vocab_size']
-    memsize = metadata['clen']
-    sentence_size = metadata['slen']
+        # gather info from metadata
+        num_candidates = metadata['candidates']['vocab_size']
+        vocab_size = metadata['vocab_size']
+        memsize = metadata['clen']
+        sentence_size = metadata['slen']
+        qlen = metadata['qlen']
 
-    # build data format
-    dformat = [ 'contexts', 'questions', 'answers' ]
+        print(':: <task {}> memory size : {}'.format(task, memsize))
 
-    # create feeds
-    trainfeed = DataFeed(dformat, data=data['train'])
-    testfeed  = DataFeed(dformat, data=data['test' ])
+        # build data format
+        dformat = [ 'contexts', 'questions', 'answers' ]
 
-    # instantiate model
-    model = MemoryNet(hdim=20, num_hops=3, memsize=memsize, 
-                      sentence_size=sentence_size, 
-                      vocab_size=vocab_size,
-                      num_candidates = num_candidates,
-                      lr = 0.001)
+        # create feeds
+        trainfeed = DataFeed(dformat, data=data['train'])
+        testfeed  = DataFeed(dformat, data=data['test' ])
 
-    with tf.Session() as sess:
-        # init session
-        sess.run(tf.global_variables_initializer())
+        # instantiate model
+        model = MemoryNet(hdim=20, num_hops=3, memsize=memsize,
+                          sentence_size=sentence_size,
+                          qlen=qlen,
+                          vocab_size=vocab_size,
+                          num_candidates = num_candidates)
 
-        # create trainer
-        trainer = Trainer(sess, model, trainfeed, testfeed,
-                batch_size = batch_size)
+        with tf.Session() as sess:
+            # run for multiple initializations
+            i, accuracy = 0, [0.]
+            while accuracy[-1] < 0.95 and i < 5:
+                # init session
+                sess.run(tf.global_variables_initializer())
 
-        print('\n:: [1/2] Pretraining')
-        # pretrain
-        trainer.fit(epochs=100000, eval_interval=100, 
-                mode=Trainer.PRETRAIN, verbose=False)
-        
-        print('\n:: [1/2] Training')
-        # train
-        trainer.fit(epochs=10000, eval_interval=100, 
-                mode=Trainer.TRAIN, verbose=False)
+                # create trainer
+                trainer = Trainer(sess, model, trainfeed, testfeed,
+                        batch_size = batch_size)
+
+                print('\n:: <task {}> ({}) [1/2] Pretraining'.format(task, i))
+                # pretrain
+                acc = trainer.fit(epochs=100000, eval_interval=1,
+                            mode=Trainer.PRETRAIN, verbose=False,
+                            batch_size=64, lr=0.0005)
+                print(':: \tAccuracy after pretraining: ', acc)
+                
+                print('\n:: <task {}> ({}) [2/2] Training'.format(task, i))
+                # train
+                acc = trainer.fit(epochs=1000000, eval_interval=100,
+                            mode=Trainer.TRAIN, verbose=False,
+                            batch_size=64, lr=0.0005)
+                print(':: \tAccuracy after training: ', acc)
+
+                # next iteration
+                i += 1
+                # add accuracy to list
+                accuracy.append(acc)
+                print(acc)
+
+            print('Experiment Results : ')
+            for i, a in enumerate(accuracy[1:]):
+                print(i, a)
+
+        task_max_acc.append(max(accuracy))
+
+    print('____________________________________________')
+    for i, acc in enumerate(task_max_acc):
+        print('Task ({}) : {}'.format(i+1, acc))
+    print('____________________________________________')
