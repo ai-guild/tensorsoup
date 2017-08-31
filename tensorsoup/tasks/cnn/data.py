e9dfedd9c03eadae0d9147f71f8761da4fc1c750
M Selvakumar <selva.developer@gmail.com>
2017-08-31 14:45:00 +0530

    DataLoader builds DataFeed for facilitating batch creation

--- tensorsoup/tasks/cnn/data.py
+++ tensorsoup/tasks/cnn/data.py
@@ -7,7 +7,38 @@ sys.path.append('../../')
 TRAIN, TEST, VALID = 'training', 'test', 'validation'
 DSETS = TRAIN, TEST, VALID
 
-from tasks.cnn.proc import load_data, buildDictionary
+from proc import load_data, buildDictionary
+from datafeed import DataFeed
+
+
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    version = 1,
+    formatters = {
+        'f': {'format':
+              '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
+        },
+    handlers = {
+        'h': {'class': 'logging.StreamHandler',
+              'formatter': 'f',
+              'level': logging.DEBUG}
+        },
+    root = {
+        'handlers': ['h'],
+        'level': logging.DEBUG,
+        },
+)
+
+dictConfig(logging_config)
+
+log = logging.getLogger('tasks.cnn.proc')
+
+print('info>> logging works')
+
+from pprint import pprint, pformat
+
 
 class DataLoader(object):
     '''
@@ -20,35 +51,41 @@ class DataLoader(object):
     def __init__(self,
                  rootdir = '../../../datasets/cnn',
                  dsets   = ['training', 'test'],
-                 dformat = ['contexts', 'questions', 'answers'],
+                 dformat = ['contexts', 'questions', 'candidates', 'answers'],
+                 nsamples = 10000,
+                 max_length = 30,
                  windows = None
     ):
               
         self.rootdir = rootdir
         self.dformat = dformat
+        self.max_length = max_length
 
         self.data = {}
 
         for dset in dsets:
-            self.data[dset] = load_data(rootdir, dset)
+            self.data[dset] = load_data(rootdir, dset, dformat=dformat, nsamples=nsamples)
 
         self.build_vocab()
         self.build_vectors()
 
     def build_vocab(self, vocabs = ['contexts', 'answers', 'candidates', 'questions']):
         # Build vocabulary for each fields and also a global one
-        self.vocab = {'global' : buildDictionary(['PAD', 'UNK', '<eos>'], [])}
+        self.vocab = {'global' : buildDictionary(['PAD', 'UNK', '<eos>'])}
 
         for dkey, dval in self.data.items():
+
             for field in dval.keys():
-                
+                print('info>> building vocabulary for {}.{}'.format(dkey, field))                
                 if field not in vocabs:
                     continue
                 
                 if field in self.vocab.keys():
-                    self.vocab[field] += buildDictionary(['PAD', 'UNK', '<eos>'], dval[field])
+                    self.vocab[field] += buildDictionary(['PAD', 'UNK', '<eos>'],
+                                                         self.max_length, dval[field])
                 else:
-                    self.vocab[field]  = buildDictionary(['PAD', 'UNK', '<eos>'], dval[field])
+                    self.vocab[field]  = buildDictionary(['PAD', 'UNK', '<eos>'],
+                                                          self.max_length, dval[field])
 
                 self.vocab['global'] += self.vocab[field]
 
@@ -69,12 +106,13 @@ class DataLoader(object):
             
             for field in set(self.data[dset].keys()) & set(self.vocab_pairs.keys()):
                 corr_vocab = self.vocab_pairs[field]
+                print('info>> building vectors for {}.{} from {}'.format(dset, field, corr_vocab))                
                 print(field, corr_vocab)
                 self.vdata[dset][field] = vectorize_tree(self.data[dset][field],
                                                          self.vocab[corr_vocab])
             
     def __call__(self, dset='training'):
-        return DataFeed(dformat, self.vdata[dset])
+        return DataFeed(self.dformat, self.vdata[dset])
 
     def lookup(self, ids, vocab='test'):
         return [self.vocab[vocab].idx2word[i] for i in ids]
@@ -84,6 +122,44 @@ def vectorize_tree(node, vocab):
     if type(node) == type([]):
         return [ vectorize_tree(u, vocab) for u in node ]
     elif type(node) == type('string'):
-        return vocab.word2idx[node]
-
+        if node in vocab.word2idx:
+            return vocab.word2idx[node]
+        else :
+            return vocab.word2idx['UNK']
+        
+if __name__ == '__main__':
+    loader = DataLoader(dsets=[TEST, TRAIN], nsamples=100)
+    print( loader.data[TEST]['questions'][0], loader.lookup(loader.vdata[TEST]['questions'][0], 'global') )
     
+    for dset in loader.vdata.keys():
+        #answer - single word
+        for answer in loader.vdata[dset]['answers']:
+            assert len(answer) == 1, 'answer "{}" not single token'.format(answer)
+            
+        #num of examples match across data and vdata
+        for field in loader.vdata[dset].keys():
+            print('{}.{} len --> {}'.format(dset, field, len(loader.vdata[dset][field])))
+            assert len(loader.data[dset][field]) == len(loader.vdata[dset][field]), 'failed: num of samples not equal {} {} -- {} != {}'.format(dset, field,
+                                                                                                                                            len(loader.data[dset][field]),
+                                                                                                                                            len(loader.vdata[dset][field]))
+        #all words are in global vocab
+        for field in loader.data[dset].keys():
+            for sample in loader.data[dset][field]:
+                for word in sample:
+                    if len(word) > loader.max_length:
+                        continue
+                    assert word in loader.vocab['global'].idx2word,'{} from {}.{}not in global vocab'.format(word, dset, field)
+                
+
+    del loader.data
+    
+    train_feed = loader()
+    c1, q, c2, a = train_feed.next_batch(batch_size=10)
+    print('##############################')
+    print(len(c1), c1)
+    print('##############################')
+    print(len(q), q)
+    print('##############################')
+    print(len(c2), c2)
+    print('##############################')
+    print(len(a), a)
