16c95516f71ab78bf5714de9615d6a16560fdc32
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2017-09-04 19:34:02 +0530

    create mask of context based on candidates (cbt)

---
+++ tensorsoup/tasks/cbt/modules.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# context -> paddeded numpy array [N, clen]
+# candidates -> [N, 10]
+def candidate_mask(contexts, candidates):
+    
+    mask = []
+    for context, candidates_i in zip(contexts, candidates):
+        mask_i = []
+        for candidate in candidates_i:
+            mask_i.append(context == candidate)
+        mask.append(mask_i)
+
+    return np.array(mask, dtype=np.int32)
